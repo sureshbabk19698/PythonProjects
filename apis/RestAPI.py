9f31f7f1e0a67a645e878fd7e5eda5db588650a5
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-01-26 20:35:58 +0530

    DataFrame Basics- Changes

--- apis/RestAPI.py
+++ apis/RestAPI.py
@@ -1,5 +1,6 @@
-import requests
 import ssl
+
+import requests
 
 print(ssl.OPENSSL_VERSION)
 
@@ -7,9 +8,13 @@ api_url = "http://localhost:8080/notification/updateTodaysFeed"
 
 
 class NotificationModel:
-    def __init__(self, headline, content):
+    def __init__(self, feedId, headline, content):
+        self.feedId = feedId
         self.headline = headline
         self.content = content
+
+    def __str__(self):
+        return f"{self.feedId}({self.headline})({self.content})"
 
 
 notificationList = [{"headline": "CR7", "content": "won the world cup"}]
@@ -17,3 +22,9 @@ notificationList = [{"headline": "CR7", "content": "won the world cup"}]
 response = requests.post(url=api_url, json=notificationList, verify=False,
                          headers={"Content-Type": "application/json"})
 print(response.json())
+
+for x in response.json():
+    del x['lastEditedDate']
+    print(NotificationModel(**x))
+# x = json.loads(response.json(), object_hook=lambda d: SimpleNamespace(**d))
+# print(x)
